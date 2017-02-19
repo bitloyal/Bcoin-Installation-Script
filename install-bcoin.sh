8396df762b453a7a98e6c72bae1b1077c7409421
Michael English <michael@michael-Sager>
2017-02-19 16:36:10 -0600

    fix typos in comments

--- install-bcoin.sh
+++ install-bcoin.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Bash script to install Bcoin onto a Debian or Ubuntu system using git.
+# Bash script to install Bcoin onto a Debian or Ubuntu system using Git.
 #
 # Run as root:
-# sudo bash bcoin.sh
+# sudo bash install-bcoin.sh
 
 DISTRO="$(lsb_release -s -c)"
 NODE_VERSION=node_6.x
 BCOIN_REPO=git://github.com/bcoin-org/bcoin.git
 BCOIN_VERSION=v1.0.0-beta
 
-# Add Node.js Repository
+# Add Node.js repository
 wget -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 echo "deb http://deb.nodesource.com/$NODE_VERSION $DISTRO main" | tee /etc/apt/sources.list.d/nodesource.list
 echo "deb-src http://deb.nodesource.com/$NODE_VERSION $DISTRO main" | tee -a /etc/apt/sources.list.d/nodesource.list
@@ -19,12 +19,12 @@ echo "deb-src http://deb.nodesource.com/$NODE_VERSION $DISTRO main" | tee -a /et
 apt update
 apt install -y nodejs build-essential git
 
-# Download BCoin
+# Download Bcoin
 git clone $BCOIN_REPO
 cd bcoin
 git checkout $BCOIN_VERSION
 
-# Install BCoin
+# Install Bcoin globally
 npm install -g
 
 # Clean up
